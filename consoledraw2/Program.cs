825dbd20f34e40d695df63d1294a24cf46d0a85a
hossein <1383.hosseinaskari@gmail.com>
2021-08-18 18:35:27 +0430

    my project

---
+++ consoledraw2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoledraw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("            °");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("            │");
+            Console.WriteLine("            │");
+            Console.WriteLine("            │");
+            Console.WriteLine("            │");
+            Console.WriteLine("            █");
+            Console.WriteLine("            █");
+            Console.WriteLine("            █");
+            Console.WriteLine("            █");
+            Console.WriteLine("           ███");
+            Console.WriteLine("           ███");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("           ███");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("          █████");
+            Console.WriteLine("        █████████");
+            Console.WriteLine("      █████████████");
+            Console.WriteLine("     ███████████████");
+            Console.WriteLine("   ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");    
+            Console.WriteLine("    █████████████████");
+            Console.WriteLine("      █████████████");
+            Console.WriteLine("       ███████████");
+            Console.WriteLine("        █████████");
+            Console.WriteLine("         ███████");
+            Console.WriteLine("           ███");
+            Console.WriteLine("         ███████");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("         ▌▌███▐▐");
+            Console.WriteLine("█████████████████████████");
+            Console.WriteLine(" ███████████████████████");
+            Console.WriteLine("  █████████████████████");
+            Console.WriteLine("   ███████████████████");
+            Console.WriteLine("    █████████████████");
+            Console.ReadKey();
+
+        }
+    }
+}
